a8119fc0ce096d94ba4887eccf9e290c2bb12b02
Brandon Cone <bcone@esu10.org>
2017-10-03 14:36:10 -0500

    Adding some output to show when container is running

--- container_files/etc/scripts/git_commands.sh
+++ container_files/etc/scripts/git_commands.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/with-contenv /bin/bash
 
+
+
 if [ -d .git ]; then
     echo "Git repo exists in this directory"
-    git add . && \
-    git commit -m 'Nightly Commit for $(date)'
+    # git add . && \
+    # git commit -m "Nightly Commit for $(date)"
 else
-    echo "No git directory in $(ls)"
+    echo "No git directory in $(pwd)"
 fi
