53e08c048a0637c186ef5a9dd67be3a20546f64e
Brandon Cone <bcone@esu10.org>
2017-10-03 10:47:31 -0500

    Getting the script set up and running

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 #Install and configure [] 
 
-FROM project42/wordpress
+FROM project42/s6-centos:centos7
 MAINTAINER Brandon Cone bcone@esu10.org
 
 COPY container_files /
 
-RUN 
+WORKDIR /var/www/html/wordpress/wp-content
+
+# RUN git add . && \
+# git commit -m 'Nightly Commit for $(date)'
+
+RUN yum install -y git
--- container_files/etc/cont-init.d/git_commands.sh
+++ container_files/etc/cont-init.d/git_commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv /bin/bash
 
-cd /var/www/html/wordpress/wp_content
-git add .
-git commit -m "Test Commit at $(date)"
-
-# echo "Test Commit at $(date)"
+# Check if git is installed and if not, install it.
+yum install git && \
+cd /var/www/html/wordpress/wp-content &&\
+git add . && \
+git commit -m 'Nightly Commit at $(time)'
