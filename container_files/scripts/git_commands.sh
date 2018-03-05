d57bc3291c8cc28da04296ed5799acfbf22f1027
Brandon <bcone@esu10.org>
2018-03-04 21:41:17 -0600

    add some logic to push/pull

--- container_files/scripts/git_commands.sh
+++ container_files/scripts/git_commands.sh
@@ -15,9 +15,17 @@ if [ -d .git ]; then
     echo "Git repo exists in this directory"
     git add . --all
     git commit -m "Nightly Commit for $(date)"
+    if [ ! -z (git remote -v) ]; then
+        git push origin master
+    fi
 else
     echo "No git directory in $(pwd)"
     git init
     git config --global user.name "Docker Sidekick Container"
     git config --global user.email "bcone+docker_sidekick@esu10.org"
+    if [ ! -z GIT_URL ]; then
+        echo "Remote url supplied, adding origin."
+        git remote add origin ${GIR_URL}
+        git pull origin master
+    fi
 fi
